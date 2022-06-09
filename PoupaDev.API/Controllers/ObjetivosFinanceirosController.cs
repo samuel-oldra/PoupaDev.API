727feb00d3073ea627e3b15149517fd7d7f883e8
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-06-09 00:00:00 -0300

    Adicionar comentários no SWAGGER - UI #1

--- PoupaDev.API/Controllers/ObjetivosFinanceirosController.cs
+++ PoupaDev.API/Controllers/ObjetivosFinanceirosController.cs
@@ -15,7 +15,14 @@ namespace PoupaDev.API.Controllers
         public ObjetivosFinanceirosController(PoupaDevDbContext context) =>
             _context = context;
 
+        // GET: api/objetivos-financeiros
+        /// <summary>
+        /// Listagem de Objetivos Financeiros
+        /// </summary>
+        /// <returns>Lista de Objetivos Financeiros</returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetTodos()
         {
             var objetivos = _context.Objetivos;
@@ -23,18 +30,46 @@ namespace PoupaDev.API.Controllers
             return Ok(objetivos);
         }
 
+        // GET: api/objetivos-financeiros/{id}
+        /// <summary>
+        /// Detalhes do Objetivo Financeiro
+        /// </summary>
+        /// <param name="id">ID do Objetivo Financeiro</param>
+        /// <returns>Mostra um Objetivo Financeiro</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetPorId(int id)
         {
             var objetivo = _context.Objetivos.Include(o => o.Operacoes).SingleOrDefault(s => s.Id == id);
 
             if (objetivo == null)
-                return NotFound();
+                return NotFound("Objetivo Financeiro não encontrado.");
 
             return Ok(objetivo);
         }
 
+        // POST: api/objetivos-financeiros
+        /// <summary>
+        /// Cadastro de Objetivo Financeiro
+        /// </summary>
+        /// <remarks>
+        /// Requisição:
+        /// {
+        ///     "titulo": "Viagem para Orlando",
+        ///     "descricao": "Economizar para viajar para Orlando",
+        ///     "valorObjetivo": 15000
+        /// }
+        /// </remarks>
+        /// <param name="model">Dados do Objetivo Financeiro</param>
+        /// <returns>Objeto criado</returns>
+        /// <response code="201">Sucesso</response>
+        /// <response code="400">Dados inválidos</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(ObjetivoFinanceiroInputModel model)
         {
             var objetivo = new ObjetivoFinanceiro(model.Titulo, model.Descricao, model.ValorObjetivo);
@@ -45,7 +80,26 @@ namespace PoupaDev.API.Controllers
             return CreatedAtAction("GetPorId", new { objetivo.Id }, model);
         }
 
+        // POST: api/objetivos-financeiros/{id}/operacoes
+        /// <summary>
+        /// Cadastro de Operação Financeira
+        /// </summary>
+        /// <remarks>
+        /// Requisição:
+        /// {
+        ///     "valor": 100,
+        ///     "tipoOperacao": 1
+        /// }
+        /// </remarks>
+        /// <param name="id">ID do Objetivo Financeiro</param>
+        /// <param name="model">Dados da Operação Financeira</param>
+        /// <response code="204">Sucesso</response>
+        /// <response code="400">Dados inválidos</response>
+        /// <response code="404">Não encontrado</response>
         [HttpPost("{id}/operacoes")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult PostOperacao(int id, OperacaoInputModel model)
         {
             var operacao = new Operacao(model.Valor, model.TipoOperacao, id);
@@ -53,7 +107,7 @@ namespace PoupaDev.API.Controllers
             var objetivo = _context.Objetivos.Include(o => o.Operacoes).SingleOrDefault(o => o.Id == id);
 
             if (objetivo == null)
-                return NotFound();
+                return NotFound("Objetivo Financeiro não encontrado.");
 
             objetivo.RealizarOperacao(operacao);
 
--- PoupaDev.API/Program.cs
+++ PoupaDev.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PoupaDev.API.Jobs;
 using PoupaDev.API.Persistence;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +19,24 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "PoupaDev.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Samuel B. Oldra",
+            Email = "samuel.oldra@gmail.com",
+            Url = new Uri("https://github.com/samuel-oldra")
+        }
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    o.IncludeXmlComments(xmlPath);
+});
 
 // Job - Hosted Services
 builder.Services.AddHostedService<RendimentoAutomaticoJob>();
@@ -29,7 +48,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(o =>
+    {
+        o.RoutePrefix = string.Empty;
+        o.SwaggerEndpoint("/swagger/v1/swagger.json", "DevGames.API v1");
+    });
 }
 else
 {
